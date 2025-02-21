7b0c48ea780b62f787c52b82355420bcb18c3637
abdelrahmanHamdyG <dhamdy078@gmail.com>
2025-02-21 13:38:59 -0500

    fixing bugs

--- a.py
+++
@@ -1,9 +0,0 @@
-import cohere
-
-co = cohere.Client("XL1OoRhBkXTTfeXYNiDF2wFhLFCQxJZhKkzxNmlO")
-response = co.embed(
-    model="embed-english-v3.0",
-    texts=["Hello, world!"],
-    input_type="search_document",
-)
-print(response.embeddings)
